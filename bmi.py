c76143b6f328fe74855415e406f4988329a350d4
tomassabol <tomassabol2@gmail.com>
2020-10-24 22:48:27 +0200

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,10 +1,16 @@
-#uloha #5 - \bmi.py
-
 h = int(input("vložte výšku v CM a stlačte ENTER"))
-w  = int(input("vložte váhu v kg a stlačte ENTER"))
+w = int(input("vložte váhu v kg a stlačte ENTER"))
 
 def BMI():
     a= w/((h/100)*(h/100))
-    return round(a,2)
+    if a <= 18.5:
+        print("Podváha")
+    elif a >= 18.5 and a <= 24.9:
+        print("Normálna hmotnosť")
+    elif a >= 25 and a <= 29.9:
+        print("Nadváha")
+    else:
+        print("Obezita")
+    return round(a, 2)
 
 print("BMI pri výške", h, "m a hmotnosti", w, "kg je", BMI())
